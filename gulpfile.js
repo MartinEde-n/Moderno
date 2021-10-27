3e10d48d7e3a737e43e43b865e80a125806fdc04
MartinEde-n <evgen.vezstuden@gmail.com>
2021-10-27 21:59:16 +0300

    added product-slider and  product

--- gulpfile.js
+++ gulpfile.js
@@ -8,13 +8,13 @@ let gulp = require('gulp'),
 		cssmin = require('gulp-cssmin');
 
 gulp.task('sass',function(){
-	return gulp.src('../Moderno/app/scss/style.scss')
-					.pipe(sass({outputStyle: 'compressed'}))
+	return gulp.src('app/scss/style.scss')
+					.pipe(sass({outputStyle: 'expanded'}))
 					.pipe(rename({suffix:'.min'}))
 					.pipe(autoprefixer({
 						overrideBrowserslist:['last 8 versions']
 					}))
-					.pipe(gulp.dest('../Moderno/app'))
+					.pipe(gulp.dest('app'))
 					.pipe(browserSync.reload({stream: true}))
 });
 
@@ -22,8 +22,9 @@ gulp.task('style',function(){
 	return gulp.src([
 		'node_modules/normalize.css/normalize.css',
 		'node_modules/slick-carousel/slick/slick.css',
-		'node_modules/magnific-popup/dist/magnific-popup.css'
-	])
+		'node_modules/magnific-popup/dist/magnific-popup.css',
+		'node_modules/rateyo/src/jquery.rateyo.css'
+		])
 		.pipe(concat('libs.min.css'))
 		.pipe(cssmin())
 		.pipe(gulp.dest('app/css'))
@@ -32,7 +33,9 @@ gulp.task('style',function(){
 gulp.task('script',function(){
 	return gulp.src([		
 		'node_modules/slick-carousel/slick/slick.js',
-		'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
+		'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
+		'node_modules/mixitup/dist/mixitup.js',
+		'node_modules/rateyo/src/jquery.rateyo.js'
 	])
 		.pipe(concat('libs.min.js'))
 		.pipe(uglify())
